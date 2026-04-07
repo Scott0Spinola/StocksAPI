a4893d933392f4b5a6556892c012ca33e2a5f2cb
Scott0Spinola <spinolascott17@gmail.com>
2026-04-07 17:11:49 +0100

    docker update

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c8fb5364e297b4fec8003a0f3604321a389128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd64d2009a7f12bd2a3bcb59acbfba6fbff689e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
