87fc7ee0f0ab047a2dc9664043e1c006eb845f36
Scott0Spinola <spinolascott17@gmail.com>
2026-04-29 15:22:56 +0100

    ICSharpCode.SharpZipLib.dll

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ffdfc1f673a772922b1b93f7daf523a10539b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591028f34aeea29953718ba3eb91569bc4eda3dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
