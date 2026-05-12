4954938520effa52f44179aa83ad76db9851f963
Scott0Spinola <spinolascott17@gmail.com>
2026-05-12 11:55:13 +0100

    testing

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d133215ec4e4a52eaba2403f957fc2c998da2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33ae7ebf7236d847825fbe1b338195004ae0a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
